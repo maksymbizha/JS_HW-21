ae3981626a31484650c684f31697feb5edc465db
Maksym Bizhanov <103897410+maksymbizha@users.noreply.github.com>
2023-01-22 20:07:44 +0200

    update

--- common.js
+++ common.js
@@ -1,27 +1,6 @@
 'use strict';
 
-
-// створити div
-// додати йому клас main
-
-// пофарбувати його жовтим
-
-// додати його у body
-
-// запитати у користувача колір фону, запропонувавши вибрати з 3 варіантів (варіанти придумуємо самі)
-
-// пофарбувати дів у вибраний колір. якщо колір не вибрано, то прибрати фон
-
-// запитати у користувача, чи додавати тінь
-
-// якщо користувач погодився, додати тінь для div
-
-// запитати у користувача текстове наповнення
-// додати введений текст всередину div
-
 const newDiv= document.createElement(`div`)
-newDiv.textContent= 'Div'
-
 newDiv.classList.add('main','textCent')
 newDiv.classList.add('bgYellow')
 document.body.append(newDiv)
@@ -50,10 +29,18 @@ if(userBgColor || userBgColor !== null){
     newDiv.classList.remove('bgYellow')
 }
 
+
 const userShadow= confirm('Додати тінь до div?')
 if (userShadow){
     newDiv.classList.add('shadow')
 }
 
-newDiv.textContent= prompt('Що напишемо в div?')
+
+
+const userText = prompt('Що напишемо в div?')
+if(userText){
+    newDiv.textContent = userText
+}else {
+    newDiv.textContent = 'Div'
+}
 
